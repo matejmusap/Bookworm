ee74ac597e8820c7804ae3e2bbf85f9d38c3be41
matejmusap <matej.musap@gmail.com>
2018-12-19 23:02:34 +0100

    make_sync_request

--- bookworm-frontend/src/components/pages/LoginPage.js
+++ bookworm-frontend/src/components/pages/LoginPage.js
@@ -1,10 +1,12 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
 import LoginForm from "../forms/LoginForm";
+import { login } from "../../actions/auth";
 
 class LoginPage extends React.Component {
-  submit = data => {
-    console.log(data);
-  };
+  submit = data =>
+    this.props.login(data).then(() => this.props.history.push("/"));
   render() {
     return (
       <div>
@@ -22,4 +24,12 @@ class LoginPage extends React.Component {
 //   </div>
 // );
 
-export default LoginPage;
+LoginPage.propTypes = {
+  history: PropTypes.shape({ push: PropTypes.func.isRequired }).isRequired,
+  login: PropTypes.func.isRequired
+};
+
+export default connect(
+  null,
+  { login }
+)(LoginPage);
