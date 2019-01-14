e6770e3691c0b66aa42025780f03ac31d814b090
matejmusap <matej.musap@gmail.com>
2019-01-14 21:43:31 +0100

    err_fix2

--- bookworm-backend/src/models/Book.js
+++ bookworm-backend/src/models/Book.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const schema = new mongoose.Schema({
   title: { type: String, required: true },
-  author: { type: String, required: true },
+  authors: { type: String, required: true },
   goodreadsId: { type: String },
   pages: { type: Number, required: true },
   userId: { type: mongoose.Schema.Types.ObjectId, required: true }
